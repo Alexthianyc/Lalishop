893b9ac53c2124bde3048038965c03260bb4f14d
Cristian Guardado <crissge2016@gmail.com>
2022-09-27 10:41:11 -0600

    Intento de resolucion de problemas

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
 			'@styles': path.resolve(__dirname, './src/styles/'),
 			'@icons': path.resolve(__dirname, './src/assets/icons/'),
 			'@logos': path.resolve(__dirname, './src/assets/logos/'),
+			'@routes': path.resolve(__dirname, './src/routes/'),
 		}
     },
     module: { // loaders para cada tipo de archivo
