33b47a8f018154c3511a688d7068a69d4f7b0619
Cristian Guardado <crissge2016@gmail.com>
2022-09-27 10:37:52 -0600

    Intento de resolucion de problemas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './routes/App';
+import App from './App';
 
 ReactDOM.render(< App />, document.getElementById('app'));
