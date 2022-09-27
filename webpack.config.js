6fdb8da32688e18daecccbf7c78b60862160585a
Cristian Guardado <crissge2016@gmail.com>
2022-09-27 12:11:28 -0600

    Resolucion de problema-v1

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 			'@containers': path.resolve(__dirname, './src/containers'),
 			'@styles': path.resolve(__dirname, './src/styles'),
 			'@icons': path.resolve(__dirname, './src/assets/icons'),
-			'@logos': path.resolve(__dirname, './src/assets/logos'),
+			'@logos': path.resolve(__dirname, './src/assets/logos')
 		}
     },
     module: { // loaders para cada tipo de archivo
