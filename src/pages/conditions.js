b74b21efe311e9942178f5e84259ea4c4e37c9d4
Cristian Guardado <crissge2016@gmail.com>
2022-11-06 23:00:41 -0600

    Condiciones actualizadas

--- src/pages/conditions.js
+++ src/pages/conditions.js
@@ -26,33 +26,32 @@ const Conditions = () => {
             “balizas web”, las “etiquetas” y los “píxeles” son archivos electrónicos utilizados para registrar
             información sobre cómo navega usted por el Sitio. Además, cuando hace una compra o intenta hacer una compra
             a través del Sitio, recopilamos cierta información de usted, entre la que se incluye su nombre, dirección de
-            facturación, dirección de envío, información de pago (incluidos los números de la tarjeta de crédito
-            [[INSERTAR CUALQUIER OTRO TIPO DE PAGO ACEPTADO]]), dirección de correo electrónico y número de teléfono.
-            Nos referimos a esta información como “Información del pedido”. Cuando hablamos de “Información personal” en
-            la presente Política de privacidad, nos referimos tanto a la Información del dispositivo como a la
-            Información del pedido. ¿CÓMO UTILIZAMOS SU INFORMACIÓN PERSONAL? Usamos la Información del pedido que
-            recopilamos en general para preparar los pedidos realizados a través del Sitio (incluido el procesamiento de
-            su información de pago, la organización de los envíos y la entrega de facturas y/o confirmaciones de
-            pedido). Además, utilizamos esta Información del pedido para: comunicarnos con usted; examinar nuestros
-            pedidos en busca de fraudes o riesgos potenciales; y cuando de acuerdo con las preferencias que usted
-            compartió con nosotros, le proporcionamos información o publicidad relacionada con nuestros productos o
-            servicios. Utilizamos la Información del dispositivo que recopilamos para ayudarnos a detectar posibles
-            riesgos y fraudes (en particular, su dirección IP) y, en general, para mejorar y optimizar nuestro Sitio
-            (por ejemplo, al generar informes y estadísticas sobre cómo nuestros clientes navegan e interactúan con el
-            Sitio y para evaluar el éxito de nuestras campañas publicitarias y de marketing). COMPARTIR SU INFORMACIÓN
-            PERSONAL Compartimos su Información personal con terceros para que nos ayuden a utilizar su Información
-            personal, tal como se describió anteriormente. Por ejemplo, utilizamos la tecnología de Shopify en nuestra
-            tienda online. Puede obtener más información sobre cómo Shopify utiliza su Información personal aquí:
-            https://www.shopify.com/legal/privacy. También utilizamos Google Analytics para ayudarnos a comprender cómo
-            usan nuestros clientes el Sitio. Puede obtener más información sobre cómo Google utiliza su Información
-            personal aquí: https://www.google.com/intl/es/policies/privacy/. Puede darse de baja de Google Analytics
-            aquí: https://tools.google.com/dlpage/gaoptout. Finalmente, también podemos compartir su Información
-            personal para cumplir con las leyes y regulaciones aplicables, para responder a una citación, orden de
-            registro u otra solicitud legal de información que recibamos, o para proteger nuestros derechos. PUBLICIDAD
-            ORIENTADA POR EL COMPORTAMIENTO Como se describió anteriormente, utilizamos su Información personal para
-            proporcionarle anuncios publicitarios dirigidos o comunicaciones de marketing que creemos que pueden ser de
-            su interés. Para más información sobre cómo funciona la publicidad dirigida, puede visitar la página
-            educativa de la Iniciativa Publicitaria en la Red (NAI por sus siglas en inglés) en
+            facturación, dirección de envío, información de pago (incluidos los números de la tarjeta de crédito),
+            dirección de correo electrónico y número de teléfono. Nos referimos a esta información como “Información del
+            pedido”. Cuando hablamos de “Información personal” en la presente Política de privacidad, nos referimos
+            tanto a la Información del dispositivo como a la Información del pedido. ¿CÓMO UTILIZAMOS SU INFORMACIÓN
+            PERSONAL? Usamos la Información del pedido que recopilamos en general para preparar los pedidos realizados a
+            través del Sitio (incluido el procesamiento de su información de pago, la organización de los envíos y la
+            entrega de facturas y/o confirmaciones de pedido). Además, utilizamos esta Información del pedido para:
+            comunicarnos con usted; examinar nuestros pedidos en busca de fraudes o riesgos potenciales; y cuando de
+            acuerdo con las preferencias que usted compartió con nosotros, le proporcionamos información o publicidad
+            relacionada con nuestros productos o servicios. Utilizamos la Información del dispositivo que recopilamos
+            para ayudarnos a detectar posibles riesgos y fraudes (en particular, su dirección IP) y, en general, para
+            mejorar y optimizar nuestro Sitio (por ejemplo, al generar informes y estadísticas sobre cómo nuestros
+            clientes navegan e interactúan con el Sitio y para evaluar el éxito de nuestras campañas publicitarias y de
+            marketing). COMPARTIR SU INFORMACIÓN PERSONAL Compartimos su Información personal con terceros para que nos
+            ayuden a utilizar su Información personal, tal como se describió anteriormente. Por ejemplo, utilizamos la
+            tecnología de Shopify en nuestra tienda online. Puede obtener más información sobre cómo Shopify utiliza su
+            Información personal aquí: https://www.shopify.com/legal/privacy. También utilizamos Google Analytics para
+            ayudarnos a comprender cómo usan nuestros clientes el Sitio. Puede obtener más información sobre cómo Google
+            utiliza su Información personal aquí: https://www.google.com/intl/es/policies/privacy/. Puede darse de baja
+            de Google Analytics aquí: https://tools.google.com/dlpage/gaoptout. Finalmente, también podemos compartir su
+            Información personal para cumplir con las leyes y regulaciones aplicables, para responder a una citación,
+            orden de registro u otra solicitud legal de información que recibamos, o para proteger nuestros derechos.
+            PUBLICIDAD ORIENTADA POR EL COMPORTAMIENTO Como se describió anteriormente, utilizamos su Información
+            personal para proporcionarle anuncios publicitarios dirigidos o comunicaciones de marketing que creemos que
+            pueden ser de su interés. Para más información sobre cómo funciona la publicidad dirigida, puede visitar la
+            página educativa de la Iniciativa Publicitaria en la Red (NAI por sus siglas en inglés) en
             http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work. Puede darse de baja de
             la publicidad dirigida mediante los siguientes enlaces: ALGUNOS ENLACES FRECUENTES INCLUYEN: FACEBOOK:
             https://www.facebook.com/settings/?tab=ads GOOGLE: https://adssettings.google.com/authenticated?hl=es BING:
@@ -69,10 +68,10 @@ const Conditions = () => {
             información se transferirá fuera de Europa, incluidos Canadá y los Estados Unidos. RETENCIÓN DE DATOS Cuando
             realiza un pedido a través del Sitio, mantendremos su Información del pedido para nuestros registros a menos
             que y hasta que nos pida que eliminemos esta información. MENORES El sitio no está destinado a personas
-            menores de 18. CAMBIOS Podemos actualizar esta política de privacidad periódicamente para reflejar, por
+            menores de 12 años. CAMBIOS Podemos actualizar esta política de privacidad periódicamente para reflejar, por
             ejemplo, cambios en nuestras prácticas o por otros motivos operativos, legales o reglamentarios. CONTÁCTENOS
             Para obtener más información sobre nuestras prácticas de privacidad, si tiene alguna pregunta o si desea
-            presentar una queja, contáctenos por correo electrónico a jrmauricio.jrm@gmail.com o por correo mediante el
+            presentar una queja, contáctenos por correo electrónico a lalishop_official@gmail.com por correo mediante el
             uso de la información que se proporciona a continuación: La Unión, La Unión, 3101, El Salvador
             <br />
         </div>
