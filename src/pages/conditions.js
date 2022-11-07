192be2104cf3b7a72207f30236e376e4748886dd
Cristian Guardado <crissge2016@gmail.com>
2022-11-06 22:25:39 -0600

    Condiciones repair

--- src/pages/conditions.js
+++ src/pages/conditions.js
@@ -1,130 +1,125 @@
 import React from 'react';
-import "@blueprintjs/core/lib/css/blueprint.css";
 import styles from '@styles/conditions.module.scss';
 
 const Conditions = () => {
     return (
         <div className={styles.Conditions}>
-            <code className='bp4-code-block'>
-            {
-                `Política de privacidad de Lalishop
-                Esta Política de privacidad describe cómo se recopila, utiliza y comparte su información
-                personal cuando visita o hace una compra en https://lalishop.vercel.app/ (denominado en lo
-                sucesivo el “Sitio”).
-                
+                Política de privacidad de Lalishop<br/>
+                Esta Política de privacidad describe cómo se recopila, utiliza y comparte su información<br/>
+                personal cuando visita o hace una compra en https://lalishop.vercel.app/ (denominado en lo<br/>
+                sucesivo el “Sitio”).<br/>
+                <br/>
                 INFORMACIÓN PERSONAL QUE RECOPILAMOS
-                Cuando visita el Sitio, recopilamos automáticamente cierta información sobre su dispositivo,
-                incluida información sobre su navegador web, dirección IP, zona horaria y algunas de las
-                cookies que están instaladas en su dispositivo. Además, a medida que navega por el Sitio,
-                recopilamos información sobre las páginas web individuales o los productos que ve, las
-                páginas web o los términos de búsqueda que lo remitieron al Sitio e información sobre cómo
-                interactúa usted con el Sitio. Nos referimos a esta información recopilada automáticamente
-                como “Información del dispositivo”.
-                Recopilamos Información del dispositivo mediante el uso de las siguientes tecnologías:
-                
-                COOKIES
-                Aquí tienes una lista de cookies que utilizamos. Las enlistamos para que puedas elegir si
-                quieres optar por quitarlas o no.
-                _session_id, unique token, sessional, Allows Shopify to store information about your session
-                (referrer, landing page, etc).
-                _shopify_visit, no data held, Persistent for 30 minutes from the last visit, Used by our
-                website provider’s internal stats tracker to record the number of visits
-                _shopify_uniq, no data held, expires midnight (relative to the visitor) of the next day, Counts
-                the number of visits to a store by a single customer.
-                cart, unique token, persistent for 2 weeks, Stores information about the contents of your cart.
-                _secure_session_id, unique token, sessional
-                storefront_digest, unique token, indefinite If the shop has a password, this is used to
-                determine if the current visitor has access.
-                - Los “Archivos de registro” rastrean las acciones que ocurren en el Sitio y recopilan datos,
-                incluyendo su dirección IP, tipo de navegador, proveedor de servicio de Internet, páginas de
-                referencia/salida y marcas de fecha/horario.
-                - Las “balizas web”, las “etiquetas” y los “píxeles” son archivos electrónicos utilizados para
-                registrar información sobre cómo navega usted por el Sitio.
-                Además, cuando hace una compra o intenta hacer una compra a través del Sitio,
-                recopilamos cierta información de usted, entre la que se incluye su nombre, dirección de
-                facturación, dirección de envío, información de pago (incluidos los números de la tarjeta de
-                crédito [[INSERTAR CUALQUIER OTRO TIPO DE PAGO ACEPTADO]]), dirección de
-                correo electrónico y número de teléfono. Nos referimos a esta información como
-                “Información del pedido”.
-                Cuando hablamos de “Información personal” en la presente Política de privacidad, nos
-                referimos tanto a la Información del dispositivo como a la Información del pedido.
-                
-                ¿CÓMO UTILIZAMOS SU INFORMACIÓN PERSONAL?
-                Usamos la Información del pedido que recopilamos en general para preparar los pedidos
-                realizados a través del Sitio (incluido el procesamiento de su información de pago, la
-                organización de los envíos y la entrega de facturas y/o confirmaciones de pedido). Además,
-                utilizamos esta Información del pedido para: comunicarnos con usted;
-                examinar nuestros pedidos en busca de fraudes o riesgos potenciales; y cuando de acuerdo
-                con las preferencias que usted compartió con nosotros, le proporcionamos información o
-                publicidad relacionada con nuestros productos o servicios.
-                Utilizamos la Información del dispositivo que recopilamos para ayudarnos a detectar
-                posibles riesgos y fraudes (en particular, su dirección IP) y, en general, para mejorar y
-                optimizar nuestro Sitio (por ejemplo, al generar informes y estadísticas sobre cómo nuestros
-                clientes navegan e interactúan con el Sitio y para evaluar el éxito de nuestras campañas
-                publicitarias y de marketing).
-
-                COMPARTIR SU INFORMACIÓN PERSONAL
-                Compartimos su Información personal con terceros para que nos ayuden a utilizar su
-                Información personal, tal como se describió anteriormente. Por ejemplo, utilizamos la
-                tecnología de Shopify en nuestra tienda online. Puede obtener más información sobre cómo
-                Shopify utiliza su Información personal aquí: https://www.shopify.com/legal/privacy. También
-                utilizamos Google Analytics para ayudarnos a comprender cómo usan nuestros clientes el
-                Sitio. Puede obtener más información sobre cómo Google utiliza su Información personal
-                aquí: https://www.google.com/intl/es/policies/privacy/. Puede darse de baja de Google
-                Analytics aquí: https://tools.google.com/dlpage/gaoptout.
-                Finalmente, también podemos compartir su Información personal para cumplir con las leyes
-                y regulaciones aplicables, para responder a una citación, orden de registro u otra solicitud
-                legal de información que recibamos, o para proteger nuestros derechos.
-                
-                PUBLICIDAD ORIENTADA POR EL COMPORTAMIENTO
-                Como se describió anteriormente, utilizamos su Información personal para proporcionarle
-                anuncios publicitarios dirigidos o comunicaciones de marketing que creemos que pueden
-                ser de su interés. Para más información sobre cómo funciona la publicidad dirigida, puede
-                visitar la página educativa de la Iniciativa Publicitaria en la Red ("NAI" por sus siglas en
-                inglés) en
-                http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
-                Puede darse de baja de la publicidad dirigida mediante los siguientes enlaces:
-                
-                ALGUNOS ENLACES FRECUENTES INCLUYEN:
-                FACEBOOK: https://www.facebook.com/settings/?tab=ads
-                GOOGLE: https://adssettings.google.com/authenticated?hl=es
-                BING: https://about.ads.microsoft.com/es-es/recursos/directivas/anuncios-personalizados
-                Además, puede darse de baja de algunos de estos servicios visitando el portal de exclusión
-                voluntaria de Digital Advertising Alliance
-                
-                NO RASTREAR
-                Tenga en cuenta que no alteramos las prácticas de recopilación y uso de datos de nuestro
-                Sitio cuando vemos una señal de No rastrear desde su navegador.
-                
+                Cuando visita el Sitio, recopilamos automáticamente cierta información sobre su dispositivo,<br/>
+                incluida información sobre su navegador web, dirección IP, zona horaria y algunas de las<br/>
+                cookies que están instaladas en su dispositivo. Además, a medida que navega por el Sitio,<br/>
+                recopilamos información sobre las páginas web individuales o los productos que ve, las<br/>
+                páginas web o los términos de búsqueda que lo remitieron al Sitio e información sobre cómo<br/>
+                interactúa usted con el Sitio. Nos referimos a esta información recopilada automáticamente<br/>
+                como “Información del dispositivo”.<br/>
+                Recopilamos Información del dispositivo mediante el uso de las siguientes tecnologías:<br/>
+                <br/>
+                COOKIES<br/>
+                Aquí tienes una lista de cookies que utilizamos. Las enlistamos para que puedas elegir si<br/>
+                quieres optar por quitarlas o no.<br/>
+                _session_id, unique token, sessional, Allows Shopify to store information about your session<br/>
+                (referrer, landing page, etc).<br/>
+                _shopify_visit, no data held, Persistent for 30 minutes from the last visit, Used by our<br/>
+                website provider’s internal stats tracker to record the number of visits<br/>
+                _shopify_uniq, no data held, expires midnight (relative to the visitor) of the next day, Counts<br/>
+                the number of visits to a store by a single customer.<br/>
+                cart, unique token, persistent for 2 weeks, Stores information about the contents of your cart.<br/>
+                _secure_session_id, unique token, sessional<br/>
+                storefront_digest, unique token, indefinite If the shop has a password, this is used to<br/>
+                determine if the current visitor has access.<br/>
+                - Los “Archivos de registro” rastrean las acciones que ocurren en el Sitio y recopilan datos,<br/>
+                incluyendo su dirección IP, tipo de navegador, proveedor de servicio de Internet, páginas de<br/>
+                referencia/salida y marcas de fecha/horario.<br/>
+                - Las “balizas web”, las “etiquetas” y los “píxeles” son archivos electrónicos utilizados para<br/>
+                registrar información sobre cómo navega usted por el Sitio.<br/>
+                Además, cuando hace una compra o intenta hacer una compra a través del Sitio,<br/>
+                recopilamos cierta información de usted, entre la que se incluye su nombre, dirección de<br/>
+                facturación, dirección de envío, información de pago (incluidos los números de la tarjeta de<br/>
+                crédito [[INSERTAR CUALQUIER OTRO TIPO DE PAGO ACEPTADO]]), dirección de<br/>
+                correo electrónico y número de teléfono. Nos referimos a esta información como<br/>
+                “Información del pedido”.<br/>
+                Cuando hablamos de “Información personal” en la presente Política de privacidad, nos<br/>
+                referimos tanto a la Información del dispositivo como a la Información del pedido.<br/>
+                <br/>
+                ¿CÓMO UTILIZAMOS SU INFORMACIÓN PERSONAL?<br/>
+                Usamos la Información del pedido que recopilamos en general para preparar los pedidos<br/>
+                realizados a través del Sitio (incluido el procesamiento de su información de pago, la<br/>
+                organización de los envíos y la entrega de facturas y/o confirmaciones de pedido). Además,<br/>
+                utilizamos esta Información del pedido para: comunicarnos con usted;<br/>
+                examinar nuestros pedidos en busca de fraudes o riesgos potenciales; y cuando de acuerdo<br/>
+                con las preferencias que usted compartió con nosotros, le proporcionamos información o<br/>
+                publicidad relacionada con nuestros productos o servicios.<br/>
+                Utilizamos la Información del dispositivo que recopilamos para ayudarnos a detectar<br/>
+                posibles riesgos y fraudes (en particular, su dirección IP) y, en general, para mejorar y<br/>
+                optimizar nuestro Sitio (por ejemplo, al generar informes y estadísticas sobre cómo nuestros<br/>
+                clientes navegan e interactúan con el Sitio y para evaluar el éxito de nuestras campañas<br/>
+                publicitarias y de marketing).<br/>
+                <br/>
+                COMPARTIR SU INFORMACIÓN PERSONAL<br/>
+                Compartimos su Información personal con terceros para que nos ayuden a utilizar su<br/>
+                Información personal, tal como se describió anteriormente. Por ejemplo, utilizamos la<br/>
+                tecnología de Shopify en nuestra tienda online. Puede obtener más información sobre cómo<br/>
+                Shopify utiliza su Información personal aquí: https://www.shopify.com/legal/privacy. También<br/>
+                utilizamos Google Analytics para ayudarnos a comprender cómo usan nuestros clientes el<br/>
+                Sitio. Puede obtener más información sobre cómo Google utiliza su Información personal<br/>
+                aquí: https://www.google.com/intl/es/policies/privacy/. Puede darse de baja de Google<br/>
+                Analytics aquí: https://tools.google.com/dlpage/gaoptout.<br/>
+                Finalmente, también podemos compartir su Información personal para cumplir con las leyes<br/>
+                y regulaciones aplicables, para responder a una citación, orden de registro u otra solicitud<br/>
+                legal de información que recibamos, o para proteger nuestros derechos.<br/>
+                <br/>
+                PUBLICIDAD ORIENTADA POR EL COMPORTAMIENTO<br/>
+                Como se describió anteriormente, utilizamos su Información personal para proporcionarle<br/>
+                anuncios publicitarios dirigidos o comunicaciones de marketing que creemos que pueden<br/>
+                ser de su interés. Para más información sobre cómo funciona la publicidad dirigida, puede<br/>
+                visitar la página educativa de la Iniciativa Publicitaria en la Red ("NAI" por sus siglas en<br/>
+                inglés) en<br/>
+                http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.<br/>
+                Puede darse de baja de la publicidad dirigida mediante los siguientes enlaces:<br/>
+                <br/>
+                ALGUNOS ENLACES FRECUENTES INCLUYEN:<br/>
+                FACEBOOK: https://www.facebook.com/settings/?tab=ads<br/>
+                GOOGLE: https://adssettings.google.com/authenticated?hl=es<br/>
+                BING: https://about.ads.microsoft.com/es-es/recursos/directivas/anuncios-personalizados<br/>
+                Además, puede darse de baja de algunos de estos servicios visitando el portal de exclusión<br/>
+                voluntaria de Digital Advertising Alliance<br/>
+                <br/>
+                NO RASTREAR<br/>
+                Tenga en cuenta que no alteramos las prácticas de recopilación y uso de datos de nuestro<br/>
+                Sitio cuando vemos una señal de No rastrear desde su navegador.<br/>
+                <br/>
                 SUS DERECHOS
-                Si usted es un residente europeo, tiene derecho a acceder a la información personal que
-                tenemos sobre usted y a solicitar que su información personal sea corregida, actualizada o
-                eliminada. Si desea ejercer este derecho, comuníquese con nosotros a través de la
-                información de contacto que se encuentra a continuación.
-                Además, si usted es un residente europeo, tenemos en cuenta que estamos procesando su
-                información para cumplir con los contratos que podamos tener con usted (por ejemplo, si
-                realiza un pedido a través del Sitio) o para perseguir nuestros intereses comerciales
-                legítimos enumerados anteriormente. Además, tenga en cuenta que su información se
-                transferirá fuera de Europa, incluidos Canadá y los Estados Unidos.
-                
-                RETENCIÓN DE DATOS
-                Cuando realiza un pedido a través del Sitio, mantendremos su Información del pedido para
-                nuestros registros a menos que y hasta que nos pida que eliminemos esta información.
-                MENORES
-                El sitio no está destinado a personas menores de 18.
-                
+                Si usted es un residente europeo, tiene derecho a acceder a la información personal que<br/>
+                tenemos sobre usted y a solicitar que su información personal sea corregida, actualizada o<br/>
+                eliminada. Si desea ejercer este derecho, comuníquese con nosotros a través de la<br/>
+                información de contacto que se encuentra a continuación.<br/>
+                Además, si usted es un residente europeo, tenemos en cuenta que estamos procesando su<br/>
+                información para cumplir con los contratos que podamos tener con usted (por ejemplo, si<br/>
+                realiza un pedido a través del Sitio) o para perseguir nuestros intereses comerciales<br/>
+                legítimos enumerados anteriormente. Además, tenga en cuenta que su información se<br/>
+                transferirá fuera de Europa, incluidos Canadá y los Estados Unidos.<br/>
+                <br/>
+                RETENCIÓN DE DATOS<br/>
+                Cuando realiza un pedido a través del Sitio, mantendremos su Información del pedido para<br/>
+                nuestros registros a menos que y hasta que nos pida que eliminemos esta información.<br/>
+                MENORES<br/>
+                El sitio no está destinado a personas menores de 18.<br/>
+                <br/>
                 CAMBIOS
-                Podemos actualizar esta política de privacidad periódicamente para reflejar, por ejemplo,
-                cambios en nuestras prácticas o por otros motivos operativos, legales o reglamentarios.
-                
-                CONTÁCTENOS
-                Para obtener más información sobre nuestras prácticas de privacidad, si tiene alguna
-                pregunta o si desea presentar una queja, contáctenos por correo electrónico a
-                jrmauricio.jrm@gmail.com o por correo mediante el uso de la información que se
-                proporciona a continuación:
-                La Unión, La Unión, 3101, El Salvador`
-            }
-            </code>
+                Podemos actualizar esta política de privacidad periódicamente para reflejar, por ejemplo,<br/>
+                cambios en nuestras prácticas o por otros motivos operativos, legales o reglamentarios.<br/>
+                <br/>
+                CONTÁCTENOS<br/>
+                Para obtener más información sobre nuestras prácticas de privacidad, si tiene alguna<br/>
+                pregunta o si desea presentar una queja, contáctenos por correo electrónico a<br/>
+                jrmauricio.jrm@gmail.com o por correo mediante el uso de la información que se<br/>
+                proporciona a continuación:<br/>
+                La Unión, La Unión, 3101, El Salvador<br/>
         </div>
     );
 };
